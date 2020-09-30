6b5715ca9ac98e599c8ca31d5b27a542efa53d1a
Ben <ben.walcutt@gmail.com>
2020-09-30 18:30:21 -0500

    removing target

--- target/test-classes/data.sql
+++
@@ -1,6 +0,0 @@
-insert into orders (orderId, accountId) values (1, 1);
-insert into orders (orderId, accountId) values (3, 1);
-
-insert into lineitems (lineItemId, orderId, productId, quantity) values (1, 1, 1, 1);
-
-insert into products (productId, name, price) values (1, 'Product 1', 1.00);
